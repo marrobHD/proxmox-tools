c49d86cd352a2f0b63ff9a77cf0c708ea09ae670
TechHome <marlon.otto@ghostip.tk>
2020-03-12 09:56:40 +0100

    Create restartlxc.sh

---
+++ api/lxc/restartlxc.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Date		12.03.2020
+# Author 	TechHome
+
+PROX_USERNAME=root@pam
+PROX_PASSWORD=mypassword
+HOST=myproxmoxhost
+
+NODE=mynode
+TARGET_VMID=myvmid
+
+decodeDataFromJson(){
+    echo `echo $1 \
+            | sed 's/{\"data\"\:{//g' \
+            | sed 's/\\\\\//\//g' \
+            | sed 's/[{}]//g' \
+            | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' \
+            | sed 's/\"\:\"/\|/g' \
+            | sed 's/[\,]/ /g' \
+            | sed 's/\"// g' \
+            | grep -w $2 \
+            | awk -F "|" '{print $2}'`
+}
+
+DATA=`curl -s -k -d "username=$PROX_USERNAME&password=$PROX_PASSWORD" $HOST/api2/json/access/ticket`
+TICKET=$(decodeDataFromJson $DATA 'ticket')
+CSRF=$(decodeDataFromJson $DATA 'CSRFPreventionToken')
+
+START_TASK_DATA=`curl -s -k -b "PVEAuthCookie=$TICKET" -H "CSRFPreventionToken: $CSRF" -X POST $HOST/api2/json/nodes/$NODE/lxc/$TARGET_VMID/status/reboot`
+START_TASK_RESULT=$(decodeDataFromJson $START_TASK_DATA 'data')a
