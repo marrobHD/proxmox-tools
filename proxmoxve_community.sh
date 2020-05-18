c617afa1176627f769f22b498db0badb6e723896
TechHome <marlon.otto@outlook.de>
2020-05-18 20:55:41 +0200

    Update proxmoxve_community.sh

--- proxmoxve_community.sh
+++ proxmoxve_community.sh
@@ -6,3 +6,4 @@ echo "deb http://download.proxmox.com/debian/pve $(grep "VERSION=" /etc/os-relea
 
 # Remove nag
 echo "DPkg::Post-Invoke { \"dpkg -V proxmox-widget-toolkit | grep -q '/proxmoxlib\.js$'; if [ \$? -eq 1 ]; then { echo 'Removing subscription nag from UI...'; sed -i '/data.status/{s/\!//;s/Active/NoMoreNagging/}' /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js; }; fi\"; };" > /etc/apt/apt.conf.d/no-nag-script && apt --reinstall install proxmox-widget-toolkit
+
