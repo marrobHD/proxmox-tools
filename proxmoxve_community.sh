074bbcdbb38675eca3f94cd281af00a5a40ba6f0
TechHome <marlon.otto@outlook.de>
2020-05-18 20:49:09 +0200

    Create proxmoxve_community.sh
    How to setup a community version of Proxmox VE 5.x-6.x

---
+++ proxmoxve_community.sh
@@ -0,0 +1,8 @@
+# Disable Commercial Repo
+sed -i "s/^deb/\#deb/" /etc/apt/sources.list.d/pve-enterprise.list && apt-get update
+
+# Add PVE Community Repo
+echo "deb http://download.proxmox.com/debian/pve $(grep "VERSION=" /etc/os-release | sed -n 's/.*(\(.*\)).*/\1/p') pve-no-subscription" > /etc/apt/sources.list.d/pve-no-enterprise.list && apt-get update
+
+# Remove nag
+echo "DPkg::Post-Invoke { \"dpkg -V proxmox-widget-toolkit | grep -q '/proxmoxlib\.js$'; if [ \$? -eq 1 ]; then { echo 'Removing subscription nag from UI...'; sed -i '/data.status/{s/\!//;s/Active/NoMoreNagging/}' /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js; }; fi\"; };" > /etc/apt/apt.conf.d/no-nag-script && apt --reinstall install proxmox-widget-toolkit
